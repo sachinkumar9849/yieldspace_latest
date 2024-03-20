c459cc2f4d20b34a4561e644b05a19db314e7901
sachinmadhwa <kjhasachin@gmail.com>
2024-03-20 22:41:32 +0545

    sssdfl

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(".core_team-slider").slick({
+  autoplay: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: false,
+  fade: false,
+  prevArrow:
+    "<button type='button' class='slick-prev pull-left'><i class='arrow_left' aria-hidden='true'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next pull-right'><i class='arrow_right' aria-hidden='true'></i></button>",
+});
